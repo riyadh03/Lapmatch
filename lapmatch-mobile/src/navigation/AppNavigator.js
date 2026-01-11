a1e11d449399dc789e39ec83805320ab79d77501
KhadijaBoosony <bossonykhadijae@gmail.com>
2026-01-11 23:36:09 +0100

    admin pages added yarbi salama

--- lapmatch-mobile/src/navigation/AppNavigator.js
+++ lapmatch-mobile/src/navigation/AppNavigator.js
@@ -1,24 +1,10 @@
-// import { NavigationContainer } from '@react-navigation/native';
-// import MainTabs from './MainTabs';
-
-// export default function AppNavigator() {
-//   return (
-//     <NavigationContainer>
-//       <MainTabs />
-//     </NavigationContainer>
-//   );
-// }
-
 import { NavigationContainer } from '@react-navigation/native';
 import MainTabs from './MainTabs';
-import AdminDashboard from '../screens/admin/AdminDashboard'; 
-import ManageLaptops from '../screens/admin/ManageLaptops';
-import ManageUsers from '../screens/admin/ManageUsers';
-
 
 export default function AppNavigator() {
   return (
-    <ManageUsers/>
-    // </AdminDashboard>
+    <NavigationContainer>
+      <MainTabs />
+    </NavigationContainer>
   );
 }
