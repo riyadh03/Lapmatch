c97d7932a35487fbb214f93094d9e2c821703255
riyadh03 <leprofessionnel3.0@gmail.com>
2026-01-12 13:57:23 +0100

    Icone changes

--- lapmatch-mobile/App.js
+++ lapmatch-mobile/App.js
@@ -1,5 +1,34 @@
-import AppNavigator from './src/navigation/AppNavigator';
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Image, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const t = setTimeout(() => setIsReady(true), 700);
+    return () => clearTimeout(t);
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#12122C",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={require("./assets/Lapmatch_Icone.jpg")}
+          style={{ width: 150, height: 150, borderRadius: 28, marginBottom: 24 }}
+          resizeMode="cover"
+        />
+        <ActivityIndicator size="small" color="#4953DD" />
+      </View>
+    );
+  }
+
   return <AppNavigator />;
 }
